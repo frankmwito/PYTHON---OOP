fe2fa59f7e13a83cb126916f1e79fbe8341d2b60
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-18 20:06:34 +0300

    Abstract class

--- class methods.py
+++ class methods.py
@@ -2,7 +2,7 @@
 
 class Student:
     def __init__(self, name, age, grade):
-        self.name = name
+        self.name = name 
         self.age = age
         self.grade = grade
         self.details = {}
