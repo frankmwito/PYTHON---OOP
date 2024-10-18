21cd1be645356dda32864e0c54143ca3a7eb176d
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-18 19:26:19 +0300

    static methods

--- Static methods.py
+++ Static methods.py
@@ -1,1 +1,11 @@
 # static methods
+
+class MathOperations:
+    @staticmethod
+    def sqrt(num):
+        return num ** 0.5
+ 
+num = float(input("Enter any number: "))   
+
+result = MathOperations.sqrt(num)
+print(f"The square root of {num} is {result}")
