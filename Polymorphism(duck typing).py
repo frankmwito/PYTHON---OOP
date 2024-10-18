88fab1250366aa9e66e8dc61a36a9290ce1f853b
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-18 15:09:04 +0300

    Polymorphism (duck typing)

--- Polymorphism(duck typing).py
+++ Polymorphism(duck typing).py
@@ -1,1 +1,38 @@
 # Polymorphism (duck typing)
+
+from abc import ABC,abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
+        
+    @abstractmethod
+    def speak(self):
+        pass
+    
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} woofs!!!"
+    
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} meows!!!"
+    
+    
+class Bird(Animal):
+    def speak(self):
+        return f"{self.name} chirps!!!"
+    
+
+name1 = input("Enter the Dog's name: ")    
+dog = Dog(name1)
+
+name2 = input("Enter the Cat's name: ")   
+cat = Cat(name2)
+
+name3 = input("Enter the Bird's name: ")   
+bird = Bird(name3)
+
+print(dog.speak())
+print(cat.speak())
+print(bird.speak())
